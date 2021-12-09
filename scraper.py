3096d6e959d57d4c197be13b62e04a7817d6fe4a
vyknight <knightadark101@gmail.com>
2021-12-09 16:47:53 -0500

    Updated scraper

--- scraper.py
+++ scraper.py
@@ -4,7 +4,20 @@ DEC 09 2021
 This script is used to scrape tweets from twitter for use in my csc110 final project
 The scraper accepts a range of dates and outputs the tweets into a csv file.
 Each line of data in the csv file will be of the following:
-<status id>, <status date>
+<status id>, <status date>, <content>
 
 This scraper uses the snscraper library.
 """
+import os
+import snscrape.modules.twitter as snt
+import pandas as pd
+
+
+# need to set the tweets somewhere
+tweets_list = []
+
+# scrape data
+for i, tweet in enumerate(snt.TwitterSearchScraper('Covid' or 'shitting').get_items()):
+    if i > 100:
+        break
+    tweets_list.append(tweet)
