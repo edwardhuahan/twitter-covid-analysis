8ec0d60e6a6ade2c2c05cbd4043fce08b35f3ea3
Edward Han <edwardhan@HP-Printer-19YM.local>
2021-12-09 16:24:36 -0500

    Parsed time data

--- process.py
+++ process.py
@@ -11,10 +11,10 @@ class Tweet:
         Tweet dataclass
     """
     tweet_id: int
-    date: str
+    date: datetime.datetime
     content: str
 
-    def __init__(self, tweet_id: int, date: str):
+    def __init__(self, tweet_id: int, date: datetime.datetime):
         self.tweet_id = tweet_id
         self.date = date
 
@@ -34,7 +34,28 @@ def read_tweet_data(filename: str) -> list[Tweet]:
             id = int(row[0])
             raw_date = row[1]
 
-            tweet = Tweet(id, raw_date)
+            tweet = Tweet(id, parse_time(raw_date))
             inputs_so_far.append(tweet)
 
     return inputs_so_far
+
+
+def parse_time(raw_time: str) -> datetime.datetime:
+    """
+        Take a raw unparsed date and time string as input and return a
+        datetime.datetime object
+    """
+
+    calendar = {
+        'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4,
+        'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8,
+        'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12
+    }
+
+    split_data = raw_time.split(' ')
+    split_time = [int(value) for value in split_data[3].split(':')]
+    parsed_data = datetime.datetime(day=int(split_data[2]), month=calendar[split_data[1]],
+                                    year=int(split_data[5]), hour=split_time[0],
+                                    minute=split_time[1], second=split_time[2])
+
+    return parsed_data
