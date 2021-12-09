acc568ba9c86489b707dece6f2e51dd562a183f1
vyknight <knightadark101@gmail.com>
2021-12-09 17:11:37 -0500

    Pushing onwards to saving this as a csv file.

--- scraper.py
+++ scraper.py
@@ -6,18 +6,23 @@ The scraper accepts a range of dates and outputs the tweets into a csv file.
 Each line of data in the csv file will be of the following:
 <status id>, <status date>, <content>
 
-This scraper uses the snscraper library.
+This scraper uses the snscraper and pandas libraries.
 """
-import os
+
 import snscrape.modules.twitter as snt
 import pandas as pd
 
 
 # need to set the tweets somewhere
-tweets_list = []
+tweets_lst = []
 
 # scrape data
 for i, tweet in enumerate(snt.TwitterSearchScraper('Covid' or 'shitting').get_items()):
     if i > 100:
         break
-    tweets_list.append(tweet)
+    tweets_lst.append([tweet.id, tweet.date, tweet.content])
+
+# creating dataframe
+tweets_df = pd.DataFrame(tweets_lst, columns=['Tweet Id', 'Datetime', 'Text'])
+tweets_df.to_csv('tweets.csv', sep=',', index=False)
+
