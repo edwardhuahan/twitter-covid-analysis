64b120447cfb33e925ba86b42351dd2ddfb495db
vyknight <knightadark101@gmail.com>
2021-12-09 16:02:48 -0500

    Made docstring for the scraper

---
+++ scraper.py
@@ -0,0 +1,10 @@
+"""
+DEC 09 2021
+
+This script is used to scrape tweets from twitter for use in my csc110 final project
+The scraper accepts a range of dates and outputs the tweets into a csv file.
+Each line of data in the csv file will be of the following:
+<status id>, <status date>
+
+This scraper uses the snscraper library.
+"""
