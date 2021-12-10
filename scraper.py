be51c965f7be18c7010dccb62ef7699fdc935c7c
vyknight <knightadark101@gmail.com>
2021-12-09 20:35:34 -0500

    Wrote the csv reading head

--- scraper.py
+++ scraper.py
@@ -11,17 +11,32 @@ This scraper uses the snscraper and pandas libraries.
 
 import snscrape.modules.twitter as snt
 import pandas as pd
+import numpy as np
+import csv
+import datetime as dt
+import time
 
+from datetime import datetime, timedelta
+now = datetime.now()
+now = now.strftime('%Y-%m-%d')
+
+max_tweets = 100
+
+# setting up the csv file and writer
+# open csv file, make new one if file not found
+csvFile = open('scraper-output/scrapes.csv', mode='a', newline='', encoding='utf8')
+csvWriter = csv.writer(csvFile)
+csvWriter.writerow(['id', 'date', 'contents', ])
 
 # need to set the tweets somewhere
 tweets_lst = []
 
 # scrape data
 for i, tweet in enumerate(snt.TwitterSearchScraper('Covid' or 'shitting').get_items()):
-    if i > 100:
+    if i > 10:
         break
     tweets_lst.append([tweet.id, tweet.date, tweet.content])
 
 # creating dataframe
 tweets_df = pd.DataFrame(tweets_lst, columns=['Tweet Id', 'Datetime', 'Text'])
-tweets_df.to_csv(sep=',', index=False)
+tweets_df.to_csv(path_or_buf=None, sep=',', index=False)
