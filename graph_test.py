68c80693f4fedab556aa8469484df07ee532c641
Arvin <arvin.gingoyon@outlook.com>
2021-12-10 17:30:12 -0500

    messing around with bar graph thingy and setting it up

--- graph_test.py
+++ graph_test.py
@@ -12,12 +12,23 @@ import pandas as pd
 # fig.show()
 # # fig.write_html('my_figure.html')
 
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
+#     "Number Eaten": [2, 1, 3, 1, 3, 2]
+# })
+#
+# fig = px.bar(df, x='Fruit', y='Number Eaten', animation_frame='Fruit', animation_group='Contestant',
+#              color='Contestant', barmode='group')
+
+# Ignore this, this doesn't work
 df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
-    "Number Eaten": [2, 1, 3, 1, 3, 2],
+    'Months': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+               'September', 'October', 'November', 'December'],
+    "Topics": ["Apples", "Oranges", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'w', 'y'],
+    "Number Eaten": [10, 50, 20, 30, 100, 17, 10, 50, 20, 30, 100, 17]
 })
 
-# fig = px.bar(df, x="Fruit", y="Number Eaten", color="Contestant", barmode="group")
-fig = px.line_3d(df, x="Fruit", y="Number Eaten", color="Contestant")
+fig = px.bar(df, x='Months', y='Number Eaten', animation_frame='Months', animation_group='Topics',
+             color='Topics', range_y=[25, 90], barmode='group')
 fig.show()
