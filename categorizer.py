303c49137086d1eb9be3a3c977f4acf73938909e
vyknight <knightadark101@gmail.com>
2021-12-10 22:22:20 -0500

    Untested build of the categorizer. Need to use the convert to stem function in next build.

--- categorizer.py
+++ categorizer.py
@@ -5,8 +5,43 @@ This script is used to categorize each tweet into a certain category by its cont
 
 """
 import pandas as pd
+import csv
 
 tweets_df = pd.read_csv('scraper-output/pd-test.csv')
 
+map_topics = {'vaccine': {'vaccin', 'needle', 'vax', 'vaccina'}}
+keywords = {'vaccine', 'needle', 'vax', 'vaccina'}
+key_list = list(map_topics.keys())
+val_list = list(map_topics.values())  # this is a list of sets
+
+
+def check_word(wd: str) -> str:
+    """
+    Checks which category a word belongs to
+
+    If word is in keyword then check which topic it's in and return the topic as str
+    Else return _
+    """
+    # CHANGE THIS BY CONVERTING THE WORD TO STEM AND THEN CHECKING
+    # EFFICIENCY BABY
+    if wd in keywords:
+        for tpc in map_topics:
+            if wd in map_topics[tpc]:
+                return tpc
+    else:
+        return '_'
+
+
 for i in tweets_df.index:
-    print(tweets_df.loc[i, 'col2'])
+    contents = tweets_df.loc[i, 'col2']  # column name should be 'contents'
+    categories = []
+    #  list of categories because we're going to append tweet to several csv files if needed
+    for word in contents:
+        topic = check_word(word)
+        if topic != '_':
+            categories.append(topic)
+    for category in categories:
+        csvFile = open('scraper-output/' + category + '.csv',
+                       mode='a', newline='', encoding='utf8')
+        csvWriter = csv.writer(csvFile)
+        csvWriter.writerow([tweets_df.loc[i, 'col1']])
