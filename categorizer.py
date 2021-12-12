47e3813325af4a6e0481110c14efa06ffd03f4a0
vyknight <knightadark101@gmail.com>
2021-12-11 20:50:45 -0500

    First working version.

--- categorizer.py
+++ categorizer.py
@@ -9,12 +9,12 @@ Outputs csv file in the following format: <date>, <score>
 import pandas as pd
 import csv
 
-tweets_df = pd.read_csv('scraper-output/pd-test.csv')
+tweets_df = pd.read_csv('scraper-output/pd-keyword-test.csv')
 
-map_topics = {'vaccine': {'vaccin', 'needle', 'vax', 'vaccina'}}
-keywords = {'vaccine', 'needle', 'vax', 'vaccina'}
-key_list = list(map_topics.keys())
-val_list = list(map_topics.values())  # this is a list of sets
+map_topics = {'vaccine': {'vaccine'}, 'mask': {'mask'}}
+keywords = {'vaccine', 'mask'}
+# key_list = list(map_topics.keys())
+# val_list = list(map_topics.values())  # this is a list of sets
 
 
 def check_word(wd: str) -> str:
@@ -28,23 +28,32 @@ def check_word(wd: str) -> str:
     # EFFICIENCY BABY
     if wd in keywords:
         for tpc in map_topics:
-            if wd in map_topics[tpc]:
+            if wd in map_topics[tpc]:  # this actually won't work unless you convert word to stem
                 return tpc
     else:
         return '_'
 
 
 for i in tweets_df.index:
-    contents = tweets_df.loc[i, 'col2']  # column name should be 'contents'
+    print(f'Current iteration:{i}')
+    contents = tweets_df.loc[i, 'contents']  # column name should be 'contents'
+    contents = contents.split(' ')  # because contents starts off as a string
     categories = []
     #  list of categories because we're going to append tweet to several csv files if needed
+    print(f'Current Tweet Contents: {contents}')  # print test
+
+    # checks each word in contents
     for word in contents:
         topic = check_word(word)
+        print(f'Current word:{word} Topic of word:{topic}')  # print test
         if topic != '_':
             categories.append(topic)
+    print(f'Categories:{categories}')  # print test
+
+    # appends tweet to csv file of topic
     for category in categories:
         csvFile = open('scraper-output/' + category + '.csv',
                        mode='a', newline='', encoding='utf8')
         csvWriter = csv.writer(csvFile)
-        csvWriter.writerow([tweets_df.loc[i, 'col1']])
+        csvWriter.writerow([tweets_df.loc[i, 'contents']])  # check columns when using for reals
         csvFile.close()
