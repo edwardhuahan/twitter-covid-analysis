41a0fbc8f2335301b127bbc936f37320a737cd35
arvin <arvin.gingoyon@outlook.com>
2021-12-11 20:18:45 -0500

    set up dataframe thing to graph

--- graph_test.py
+++ graph_test.py
@@ -4,6 +4,8 @@ import plotly
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
+import tweet
+import analyzer
 
 # fig = go.Figure(
 #     data=[go.Bar(y=[2, 1, 3])],
@@ -21,14 +23,25 @@ import pandas as pd
 # fig = px.bar(df, x='Fruit', y='Number Eaten', animation_frame='Fruit', animation_group='Contestant',
 #              color='Contestant', barmode='group')
 
-# Ignore this, this doesn't work
+
+# def format_graph_data(data: list[(Tweet, score, topic)]) -> Dataframe:
+#     """ Formats tweet data post-processing and returns a pandas dataframe for graph"""
+#     dict_data = {'Sentiment': [], 'Topics': [], 'Score': []}
+#     for data in range(len(data)):
+#         if data[2] not in
+#             dict_data[''] = dict_data['Sentiment'] + []
+#
+#     graph_data = pd.DataFrame(dict_data)
+#
+#     return graph_data
+
 df = pd.DataFrame({
-    'Months': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-               'September', 'October', 'November', 'December'],
-    "Topics": ["Apples", "Oranges", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'w', 'y'],
-    "Number Eaten": [10, 50, 20, 30, 100, 17, 10, 50, 20, 30, 100, 17]
+    'Sentiment_labels': ['Negative', 'Positive', 'Compound', 'Negative', 'Positive', 'Compound'],
+    'Topics': ['Covid', 'Covid', 'Covid', 'Vaccine', 'Vaccine', 'Vaccine'],
+    'Sentiment_score': [1, 2, 1, 3, 2, 1]
 })
 
-fig = px.bar(df, x='Months', y='Number Eaten', animation_frame='Months', animation_group='Topics',
-             color='Topics', range_y=[25, 90], barmode='group')
+fig = px.bar(df, x='Topics', y='Sentiment_score', animation_frame='Sentiment_labels',
+             animation_group='Topics', range_y=[0, 5], barmode='group')
 fig.show()
+fig.write_html('my_figure.html')
