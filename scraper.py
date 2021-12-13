8619ba2b706071a2e2f528490ee4cfe22b5a5683
Edward Han <edwardhan@HP-Printer-19YM.local>
2021-12-13 12:41:42 -0700

    Refactored code to fit better with pyta

--- scraper.py
+++ scraper.py
@@ -18,13 +18,14 @@ THERE'S NO WAY AROUND THIS EXCEPT REQUEST MORE TWEETS EACH DAY TO MINIMIZE THE R
 This file is Copyright (c) 2021 Edward Han, Zekun Liu (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧, Arvin Gingoyon
 """
 
-import snscrape.modules.twitter as snt
 import csv
 from datetime import datetime, timedelta
+import snscrape.modules.twitter as snt
 
 
 def scrape(start_date: str, max_tweets: int) -> None:
-    """
+    """ Takes a start date and tweet limit and exports tweets from date range
+    to export into a CSV file containing tweet id, tweet content and tweet date.
 
     :param start_date: Str, A date in the format YYYY/MM/DD
     :param max_tweets: Int, The number of total tweets you want
@@ -48,28 +49,58 @@ def scrape(start_date: str, max_tweets: int) -> None:
 
     # setting up the csv file and writer
     # open csv file, make new one if file not found
-    csv_file = open('scraper-output/scrapes.csv', mode='a', newline='', encoding='utf8')
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['id', 'date', 'contents', ])
+    with open('scraper-output/scrapes.csv', mode='a', newline='', encoding='utf8') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['id', 'date', 'contents', ])
 
-    # scrape data
-    while start_date <= current_date:
+        # scrape data
+        while start_date <= current_date:
 
-        dt = start_date.strftime('%Y-%m-%d')
-        dtn = start_date + timedelta(days=1)
-        dtn = dtn.strftime('%Y-%m-%d')
+            dt = start_date.strftime('%Y-%m-%d')
+            dtn = next_day(start_date)
+            dtn = dtn.strftime('%Y-%m-%d')
 
-        search_term = f'covid OR coronavirus OR covid19 OR corona lang:en since:{dt} until:{dtn}'
+            search_term = f'covid OR coronavirus OR covid19' \
+                          f' OR corona lang:en since:{dt} until:{dtn}'
 
-        for i, tweet in enumerate(snt.TwitterSearchScraper(search_term).get_items()):
-            if i > tweets_per_day:
-                break
-            print(f'Found tweet number {i} for date {dt}')
-            csv_writer.writerow([tweet.id, tweet.date, tweet.content])
-        start_date += timedelta(days=1)
+            for i, tweet in enumerate(snt.TwitterSearchScraper(search_term).get_items()):
+                if i > tweets_per_day:
+                    break
+                print(f'Found tweet number {i} for date {dt}')
+                csv_writer.writerow([tweet.id, tweet.date, tweet.content])
+            start_date += timedelta(days=1)
 
-    csv_file.close()
+        csv_file.close()
+
+
+def next_day(original_date: datetime.date) -> datetime.date:
+    """ Takes a datetime and returns the day after the date specified
+    in the object. This function is needed because simply adding
+    datetime.timedelta(days=1) to datetime.datetime.now() is not testable
+
+    >>> example = datetime(2021, 12, 1)
+    >>> next_day(example)
+    datetime.datetime(2021, 12, 2, 0, 0)
+
+    """
+    return original_date + timedelta(days=1)
 
 
 if __name__ == "__main__":
+    import python_ta
+    import python_ta.contracts
+
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
+
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (Delete the "#" and space before each line.)
+    # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
+    python_ta.check_all(config={
+        'extra-imports': ['snscrape.modules.twitter', 'csv', 'datetime'],
+        'allowed-io': ['scrape'],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200']
+    })
+
     scrape('2021/12/09', 1000)  # Default values
